ff275d80bb354bc3b260cd44b176a472fa59d945
kodhus <kodhus@kodhus.com>
2018-08-11 00:47:59 +0200

    Fixing versioning in gulpfile and adding emphy

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ const rename = require('gulp-rename');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 
-const { version } = require('./package.json').version;
+const { version } = require('./package.json');
 
 gulp.task('serve', () => {
   browserSync.init({
@@ -20,18 +20,18 @@ gulp.task('serve', () => {
 
 gulp.task('sass-build', () => (
   gulp.src('src/scss/kodhus.scss')
-    .pipe(postcss([autoprefixer()]))
     .pipe(sass())
     .pipe(csso())
+    .pipe(postcss([autoprefixer()]))
     .pipe(rename({ suffix: `-${version}` }))
     .pipe(gulp.dest('dist'))
     .pipe(browserSync.stream())));
 
 gulp.task('sass-build-min', () => (
   gulp.src('src/scss/kodhus.scss')
-    .pipe(postcss([autoprefixer()]))
     .pipe(sass())
     .pipe(csso())
+    .pipe(postcss([autoprefixer()]))
     .pipe(rename({ suffix: `-${version}.min` }))
     .pipe(gulp.dest('dist'))
     .pipe(browserSync.stream())));
