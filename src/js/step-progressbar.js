e52f1f721ba5a95067cf5ac7b4ee39da718f35cb
kodhus <kodhus@kodhus.com>
2018-08-10 21:43:37 +0200

    Fixing horizontal step progress bar to work properly

--- src/js/step-progressbar.js
+++ src/js/step-progressbar.js
@@ -4,11 +4,8 @@ class StepProgressBar {
     this.element = document.querySelector('.cdt-step-progressbar');
     const children = Array.from(this.element.children);
     if (this.element.classList.contains('horizontal')) {
-      this.element.querySelectorAll('li').forEach((item, index) => {
-        item.style.width = `${100 / (children.length - 1) - 1}%`;
-        if (index === children.length - 1) {
-          item.style.width = '0';
-        }
+      this.element.querySelectorAll('li').forEach((item) => {
+        item.style.width = `${100 / (children.length - 1)}%`;
       });
     }
     this.setActiveIndex(activeIndex);
